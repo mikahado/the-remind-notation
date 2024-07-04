b2a78713ec6d5aea3bc703c6e1a1b4a099ced629
MKDO <62688240+mikahado@users.noreply.github.com>
2024-07-04 15:21:04 -0600

    removes icon code,

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,12 +7,6 @@ import '../styles/index.css'
 import { DefaultMenuStructure } from '../utils/menu-structure'
 import DarkMode from '../components/dark-mode'
 
-// The following import prevents a Font Awesome icon server-side rendering bug,
-// where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
-// Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; /* eslint-disable import/first */
 
 
 export default function Home() {
